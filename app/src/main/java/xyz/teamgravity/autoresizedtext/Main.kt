94365af213b2d9720b6b1a0043b922f7e1f21f67
raheemadamboev <vividricemaryandmax@gmail.com>
2023-01-08 20:27:11 +0500

    implement auto resized text

--- app/src/main/java/xyz/teamgravity/autoresizedtext/Main.kt
+++ app/src/main/java/xyz/teamgravity/autoresizedtext/Main.kt
@@ -3,10 +3,17 @@ package xyz.teamgravity.autoresizedtext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.autoresizedtext.ui.theme.AutoResizedTextTheme
 
 class Main : ComponentActivity() {
@@ -18,6 +25,21 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .width(100.dp)
+                                .background(Color.Gray)
+                        ) {
+                            AutoResizedText(
+                                text = "Hello World",
+                                style = MaterialTheme.typography.headlineLarge
+                            )
+                        }
+                    }
                 }
             }
         }
